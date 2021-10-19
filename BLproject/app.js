bdc7e325031d91ad2966afd48c4e30d5af2ca8f5
Omer Sasson <sassonomer123@gmail.com>
2021-10-19 14:12:53 -0700

    cleaned up app code

--- BLproject/app.js
+++ BLproject/app.js
@@ -5,41 +5,12 @@ const path = require('path');
 const nodemailer = require('nodemailer');
 const cron = require('node-cron');
 
-/*
-var firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
-
-const firebaseConfig = {
-    apiKey: "AIzaSyA0sdpMzKR-cZ2tp433iD0Tgxn5rw_I3aM",
-    authDomain: "bletters-596de.firebaseapp.com",
-    databaseURL: "https://bletters-596de.firebaseio.com",
-    projectId: "bletters-596de",
-    storageBucket: "bletters-596de.appspot.com",
-    messagingSenderId: "738256535597",
-    appId: "1:738256535597:web:8566a7f1cfd3305eacf8a4"
-  };
-
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-*/
 let email = "";
 let frequency = 1;
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 const router = express.Router();
-/*
-router.post('/selectBlogsPage.html', (req, res) => {
-    console.log(req.body);
-    res.end();
-});
-
-*/
-
-
 
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/homePage.html'));
@@ -86,7 +57,7 @@ router.get('/frequencySelectionPage.html',function(req,res){
 });
 
 router.get('/signedUp.html',function(req,res){
-cron.schedule('* * */' + frequency * 24 + '* * *', () => {
+cron.schedule('0 0 */' + frequency * 24 + ' * * *', () => {
         let mailTransporter = nodemailer.createTransport({ 
             service: 'gmail', 
             auth: { 
@@ -117,34 +88,7 @@ cron.schedule('* * */' + frequency * 24 + '* * *', () => {
         
     });
     res.sendFile(path.join(__dirname+'/signedUp.html'));
-    /*
-    let mailTransporter = nodemailer.createTransport({ 
-        service: 'gmail', 
-        auth: { 
-            user: 'blettersgroup@gmail.com', 
-            pass: 'BLETTERgroup12@'
-        } 
-    }); 
-    
-    let mailDetails = { 
-        from: 'blettersgroup@gmail.com', 
-                to: email, 
-                subject: 'BLetters Confirmation', 
-                text: 'Thanks for signing up with us!'
-            }; 
-            
-    
-
-    mailTransporter.sendMail(mailDetails, function(err, data) { 
-        if(err){
-            res.send(err.toString());
-        }
-        res.send('Email sent succesfully');
-    }); 
-    
-    res.sendFile(path.join(__dirname+'/signedUp.html'));
-    //__dirname : It will resolve to your project folder.
-    */
+  
     
 });
 
